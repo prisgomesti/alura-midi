94157d77a317b256b7c3df2a181e723d1330f977
Priscila Gomes <pris.gomes38@gmail.com>
2023-03-29 17:49:37 -0300

    melhorando o toque do som das teclas

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
+let contador = 0;
+
+
+
+while(contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log (contador);
+}
